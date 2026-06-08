00518cbc8268532e2dab36fe48f14f5457f4cc4c
S-Arafin <arafin23103@gmail.com>
2026-06-08 16:14:37 +0600

    add problem3 of day3 with the solution

--- src/Day-3/checkOddEven.ts
+++ src/Day-3/checkOddEven.ts
@@ -1,3 +1,4 @@
+// Problem 2: Check Even or Odd  [Easy]
 // Description: Write a function isEven(n) that returns true if a number is even, and false if it is odd.
 // Example:
 // Input: 4  → Output: trueInput: 7  → Output: false
