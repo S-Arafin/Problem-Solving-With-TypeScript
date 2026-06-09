bb900720d7b6370349525bd8c4fd8d764d959056
S-Arafin <arafin23103@gmail.com>
2026-06-10 01:50:03 +0600

    add problem 5 of day 4 Character Repeat Counter

---
+++ src/Day-4/CharacterRepeatCounter.ts
@@ -0,0 +1,17 @@
+// Problem 10: Count Occurrences of a Character  [Easy]
+// Description: Write a function countChar(str, char) that returns how many times a character appears in a string.
+// Example:
+// Input: 'banana', 'a'  → Output: 3
+// Hint: Use split(char).length - 1 or a loop.
+
+function countChar(str: string, char: string):number{
+    let count: number = 0;
+    for(let currentChar of str){
+        if(currentChar === char){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countChar('banana', 'a'));
