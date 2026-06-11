39a648640dc1899cc58ec9ed53326c70affa1fd4
S-Arafin <arafin23103@gmail.com>
2026-06-11 16:45:49 +0600

    add problem 4 of day 6

---
+++ src/Day-6/InvertAnObj.ts
@@ -0,0 +1,13 @@
+// Problem 19: Invert an Object  [Easy]
+// Description: Write a function invertObject(obj) that swaps the keys and values of an object.
+// Example:
+// Input: {a: 1, b: 2}  → Output: {1: 'a', 2: 'b'}
+// Hint: Use Object.entries() and reduce().
+
+function inverted(obj: {[key: string]: any}): {[key:string]:string}{
+    return Object.entries(obj).reduce((a , [key, value])=>{
+        a[value] = key
+        return a;
+    },{} as {[key: string]: string})
+}
+console.log(inverted({a: 1, b: 2}));
