f0bddae72d5715ead5a622d5db1e6a3922b7da60
S-Arafin <arafin23103@gmail.com>
2026-06-16 22:54:49 +0600

    add problem 1 of day-10

---
+++ src/Day-10/SafeJSONparse.ts
@@ -0,0 +1,13 @@
+// Problem 36: Safe JSON Parse  [Easy]
+// Description: Write a function safeJsonParse(str) that tries to parse a JSON string and returns the parsed value, or null if parsing fails — without throwing an error.
+// Example:
+// safeJsonParse('{"a":1}');  // {a: 1}safeJsonParse('bad json'); // null
+// Hint: Use try/catch inside the function.
+
+function safeJsonParse(str: string): any | null {
+  try {
+    return JSON.parse(str);
+  } catch {
+    return null;
+  }
+}
